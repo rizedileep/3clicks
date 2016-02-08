79a8aba66c5ebd9e1521c859f6cd6964049d7bfc
aragonc <aragcar@gmail.com>
2016-02-08 18:17:29 -0500

    fix clients

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@ if ( !defined('ABSPATH') )
                 <div class="g1-layout-inner">
                     <div id="owl-demo" class="owl-carousel owl-theme">
                     <?php 
-                        query_posts( array ( 'post_type' => 'clientes', 'posts_per_page' => 8));
+                        query_posts( array ( 'post_type' => 'clientes'));
                         while ( have_posts() ) : the_post(); 
                     ?>
                     <div class="item">
